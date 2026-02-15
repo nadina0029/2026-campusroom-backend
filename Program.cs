3ce86833ebaf17c5c737087b10ce52de71c0bcfb
nadina0029 <nadina.nur0029@gmail.com>
2026-02-15 20:11:34 +0700

    feat: implement database seeder and jwt config

--- Program.cs
+++ Program.cs
@@ -1,16 +1,56 @@
-using Microsoft.EntityFrameworkCore;
+using System.Text;
 using CampusRoomBackend.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// --- 1. KONFIGURASI SERVICES ---
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// A. Setup Database
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// B. Setup JWT Authentication (Keamanan)
+var jwtKey = builder.Configuration["JwtSettings:Key"];
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey!));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = key
+        };
+    });
+
 var app = builder.Build();
+
+// --- 2. EKSEKUSI SEEDER (Isi Data Dummy) ---
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    
+    // Pastikan database terbuat
+    context.Database.EnsureCreated();
+    
+    // Jalankan Seeder
+    DbSeeder.Seed(context);
+}
+
+// --- 3. PIPELINE APLIKASI ---
 
 if (app.Environment.IsDevelopment())
 {
@@ -20,6 +60,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Urutan Wajib: Authentication dulu, baru Authorization
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
